b6ff7219a6c9377f5746e81992b4c6ddda17aac0
Segundo Bartra <segundobartra@gmail.com>
2019-03-11 23:23:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,24 +27,24 @@ function checkCollision(rock) {
       const dodgerRightEdge = dodgerLeftEdge+40;
       const rockLeftEdge = positionToInteger(rock.style.left)
       const rockRightEdge = rockLeftEdge+20;
-  
+
       return (
         (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
         (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
         (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge))
     }
   }
-  
+
   function createRock(x) {
     const rock = document.createElement('div')
     rock.className = 'rock'
     rock.style.left = `${x}px`
-  
+
     var top = 0
     rock.style.top = `${top}px`
-  
+
     GAME.appendChild(rock);
-  
+
     function moveRock() {
       if(checkCollision(rock)===true){
         endGame();
@@ -59,26 +59,26 @@ function checkCollision(rock) {
         ROCKS.shift();
       }
     }
-  
+
     moveRock();
-  
+
     ROCKS.push(rock);
     return rock;
   }
-  
+
   function endGame() {
     while(ROCKS.length > 0){
       GAME.removeChild(ROCKS[0]);
       ROCKS.shift();
     }
-  
+
     clearInterval(gameInterval);
     window.removeEventListener('keydown', moveDodger);
     alert("YOU LOSE!");
     START.innerHTML = 'Play again?'
     START.style.display = 'inline'
   }
-  
+
   function moveDodger(e) {
       if (e.which === LEFT_ARROW) {
         e.stopPropagation();
@@ -91,7 +91,7 @@ function checkCollision(rock) {
         moveDodgerRight();
       }
     }
-  
+
   function moveDodgerLeft() {
     const left = positionToInteger(dodger.style.left);
     if (left > 0) {
@@ -99,7 +99,7 @@ function checkCollision(rock) {
       window.requestAnimationFrame(moveDodgerLeft);
     }
   }
-  
+
   function moveDodgerRight() {
     const left = positionToInteger(dodger.style.left);
     if (left < GAME_WIDTH - 40){
@@ -107,7 +107,7 @@ function checkCollision(rock) {
       window.requestAnimationFrame(moveDodgerRight);
     }
   }
-  
+
   /**
    * @param {string} p The position property
    * @returns {number} The position as an integer (without 'px')
@@ -115,10 +115,10 @@ function checkCollision(rock) {
   function positionToInteger(p) {
     return parseInt(p.split('px')[0]) || 0
   }
-  
+
   function start() {
     window.addEventListener('keydown', moveDodger)
-  
+
     START.style.display = 'none'
     gameInterval = setInterval(function() {
         createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
